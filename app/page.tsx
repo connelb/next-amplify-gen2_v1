cf6d9d4eeaed1eb9e33f3190fc6f4e6b7fd10764
connelb <brianjconnell@hotmail.com>
2024-01-20 20:58:55 -0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import TodoList from "@/components/TodoList";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello, Amplify 👋</h1>
+      <TodoList />
+    </>
+  );
+}
+
+export default withAuthenticator(App);
---
+++ components/ConfigureAmplify.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// import config from "@/amplifyconfiguration.json";
+import { Amplify } from "aws-amplify";
+
+// here we go
+
+// Amplify.configure(config, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+  return null;
+}
